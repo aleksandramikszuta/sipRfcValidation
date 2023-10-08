4447b4452fa5190e49da378c7241d7bfb07d35dc
Aleksandra Mikszuta <ola.mikszuta@gmail.com>
2023-10-08 21:08:10 +0200

    Print functionality (request-uri)

--- main.py
+++ main.py
@@ -2,6 +2,30 @@
 
 from optparse import OptionParser
 
+def parseFile(fileName):
+    result = {}
+    for line in open(fileName, "r"):
+        if line.find(":") != -1:
+            result[line.split(":")[0]] = "".join(line.split(":")[1:])
+        else:
+            result["body"] = line
+    return result
+
+def printFile(parsedFile):
+    result = """
+The given SIP message is a request with:
+request-uri: """
+    result += parsedFile["To"]
+    """
+        method: 
+        headers:
+            : 
+            : 
+            <...>: <...>
+        and body:
+    """
+    print(result)
+
 def main():
     parser = OptionParser()
     parser.add_option("-f", "--file", dest="file", help="File to process")
@@ -14,6 +38,15 @@ def main():
                        help="Validate RFC3261 conformity (WIP)")
 
     (options, args) = parser.parse_args()
+
+    if (options.file is None):
+        parser.print_help()
+        exit()
+    
+    parsedFile = parseFile(options.file)
+
+    if(options.print):
+        printFile(parsedFile)
 
 if __name__ == "__main__":
     main()
